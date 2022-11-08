9aa72c1be477142ff522911aaecffe66c7dae1f4
Wilksoncardoso <58814107+Wilksoncardoso@users.noreply.github.com>
2022-11-08 20:10:15 -0300

    img lg

--- components/index/index__will.vue
+++ components/index/index__will.vue
@@ -280,7 +280,7 @@
       alt="Wilkson cardoso, UX / UI"
       width="100%"
       height="100%"
-      sizes="xs:10vw sm:12vw md:30vw lg:60vw "
+      sizes="xs:10vw sm:12vw md:30vw lg:700px "
       class="will__img"
       loading="lazy"
       format="webp"
