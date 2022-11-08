eca401bf40485e790c6bcb2baa1e8a230c8fa150
Wilksoncardoso <58814107+Wilksoncardoso@users.noreply.github.com>
2022-11-08 19:49:48 -0300

    atualizacao img res

--- components/index/index__will.vue
+++ components/index/index__will.vue
@@ -280,7 +280,7 @@
       alt="Wilkson cardoso, UX / UI"
       width="100%"
       height="100%"
-      sizes=" sm:25vw md:80vw lg:100vw xl:140vw"
+      sizes=" sm:20vw md:80vw lg:100vw xl:140vw"
       class="will__img"
       loading="lazy"
       format="webp"
