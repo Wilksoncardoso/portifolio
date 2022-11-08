077474aca91b2e616f7c04063990b05499a9d4c6
Wilksoncardoso <58814107+Wilksoncardoso@users.noreply.github.com>
2022-11-08 20:13:31 -0300

    teste img

--- components/index/index__will.vue
+++ components/index/index__will.vue
@@ -280,7 +280,7 @@
       alt="Wilkson cardoso, UX / UI"
       width="100%"
       height="100%"
-      sizes="xs:10vw sm:12vw md:30vw lg:700px "
+      sizes=" sm:8vw md:20vw lg:600px xl:900px"
       class="will__img"
       loading="lazy"
       format="webp"
