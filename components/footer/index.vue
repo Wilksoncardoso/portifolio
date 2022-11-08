e1438909374cb1f37c65c8aed9f588cec9e355b1
Wilksoncardoso <58814107+Wilksoncardoso@users.noreply.github.com>
2022-11-08 19:24:31 -0300

    melhoria footer

--- components/footer/index.vue
+++ components/footer/index.vue
@@ -2,10 +2,10 @@
   <div :style="background">
     <v-container>
       <v-row class="justify-center" >
-        <v-col cols="3" sm="3" md="3" lg="1" xl="1" >
+        <v-col cols="3" sm="2" md="2" lg="1" xl="1" >
         <logo/>
         </v-col>
-        <v-col cols="9" sm="9" md="9" lg="8" xl="8" >
+        <v-col cols="9" sm="7" md="7" lg="8" xl="8" >
           <p class="mb-0">
             Todos direitos reservados
             <a
@@ -21,16 +21,22 @@
             Desenvolvido em Vue3 + NuxtJS + Vuetify
             <v-icon color="primary" dark> mdi-vuetify</v-icon>
           </p>
-          <links class=" d-sm-none " />
+          <v-divider class="my-3  d-sm-none"></v-divider>
+          <p class="text-caption white--text  d-sm-none">Me encontre, através</p>
+
+          <links class=" d-sm-none" />
 
         </v-col>
         <v-col
           cols="12"
+          md="3"
+          sm="3"
           lg="3"
           xl="3"
-          class="text-center text-xl-right text-lg-right mb-0 mt-2"
+          class="text-center text-xl-right text-lg-right mb-0 mt-2  d-md-block d-xs-none"
         >
-          <links class=" d-sm-none d-md-block " />
+        <p class="text-caption white--text mb-2">Me encontre, através</p>
+          <links  />
         </v-col>
       </v-row>
     </v-container>
